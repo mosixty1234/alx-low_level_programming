1d5ab6cf0046f3a2e8e3bce5681a7e71a195bcda
mosixty1234 <mosixtysipho11@gmail.com>
2023-05-03 12:01:24 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,32 +1,40 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a listint_t list.
- * @h: pointer to the head of the list
- * Return: the size of the list that was free'd
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
  */
-
 size_t free_listint_safe(listint_t **h)
 {
-	size_t i = 0;
-	listint_t *tmp = *h;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-	if (h == NULL)
-		exit(98);
+	if (!h || !*h)
+		return (0);
 
-	while (tmp != NULL)
+	while (*h)
 	{
-		*h = tmp->next;
-		free(tmp);
-		tmp = *h;
-		i++;
-		if (tmp <= *h)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
-			free(tmp);
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
 			break;
 		}
 	}
-	*h = NULL;
-	return (i);
-}
 
+	*h = NULL;
+
+	return (len);
+}
